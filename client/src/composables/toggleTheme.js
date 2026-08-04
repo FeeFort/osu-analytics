1268d4f3b0db2cb709f138cd8f82926830ee9f78
TraceXR1 <serzh.v.zhukov@gmail.com>
2026-08-05 02:27:02 +0300

    Start theme surfaces in sync

--- client/src/composables/toggleTheme.js
+++ client/src/composables/toggleTheme.js
@@ -6,18 +6,24 @@ const THEME_TRANSITION_DURATION = 180;
 const rootElement = document.documentElement;
 const isDark = ref(rootElement.classList.contains(DARK_THEME_CLASS));
 let transitionTimeout;
+let transitionFrame;
 
 function setTheme(useDarkTheme) {
     const nextIsDark = Boolean(useDarkTheme);
 
     clearTimeout(transitionTimeout);
+    cancelAnimationFrame(transitionFrame);
     rootElement.classList.add(THEME_TRANSITION_CLASS);
-    rootElement.classList.toggle(DARK_THEME_CLASS, nextIsDark);
-    isDark.value = nextIsDark;
+    // First paint the transition rules, then change the theme on the next
+    // frame. This gives every large surface the same transition start point.
+    transitionFrame = requestAnimationFrame(() => {
+        rootElement.classList.toggle(DARK_THEME_CLASS, nextIsDark);
+        isDark.value = nextIsDark;
 
-    transitionTimeout = window.setTimeout(() => {
-        rootElement.classList.remove(THEME_TRANSITION_CLASS);
-    }, THEME_TRANSITION_DURATION);
+        transitionTimeout = window.setTimeout(() => {
+            rootElement.classList.remove(THEME_TRANSITION_CLASS);
+        }, THEME_TRANSITION_DURATION);
+    });
 }
 
 function toggleTheme() {
